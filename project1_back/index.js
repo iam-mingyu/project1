666dafcb2922fabafe486455a487fe3b79478e62
Martin <mininnov99@kaist.ac.kr>
2022-06-29 21:38:48 +0900

    Day3 complete

--- project1_back/index.js
+++ project1_back/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const {User} = require("./models/User")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json)
+
 mongoose.connect('mongodb+srv://admin:admin01!@project1.cctsx.mongodb.net/?retryWrites=true&w=majority',{
      useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDb connected...'))
@@ -9,6 +15,19 @@ mongoose.connect('mongodb+srv://admin:admin01!@project1.cctsx.mongodb.net/?retry
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  //회원가입 할 때 필요한 정보들을 client에서 가져오면 
+  //그것들을 DB에 넣어준다. 
+
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
